5284384efdff516f337760c905a11193d70de9ea
aillusions <aillusions@gmail.com>
2017-12-01 12:43:31 +0200

    AddrGenerator - 1m - substring

--- src/main/java/trn/AddrGenerator.java
+++ src/main/java/trn/AddrGenerator.java
@@ -13,13 +13,13 @@ public class AddrGenerator {
 
     public static void main(String[] args) throws IOException {
 
-        PrintWriter writer = new PrintWriter(new FileWriter("1m_addresses.txt", true));
+        PrintWriter writer = new PrintWriter(new FileWriter("10m_addresses.txt", true));
 
-        for (int i = 0; i < 1_000_000; i++) {
+        for (int i = 0; i < 10_000_000; i++) {
             ECKey key = getNewECKey();
             String testBtcAddr = getBtcAddress(key);
             //writer.append(testBtcAddr + "\n");
-            writer.append(testBtcAddr.substring(0, 27) + "\n");
+            writer.append(testBtcAddr.substring(0, 26) + "\n");
         }
 
         writer.flush();
